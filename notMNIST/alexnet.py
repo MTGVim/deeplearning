bf8cae5a258577d466804fb79c4d2e65931420ac
das135 <31717144+das135@users.noreply.github.com>
2017-09-07 16:03:31 +0900

    Create alexnet.py

---
+++ notMNIST/alexnet.py
@@ -0,0 +1,135 @@
+import numpy as np
+import os
+from sklearn.cross_validation import train_test_split
+from PIL import Image
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.optimizers import RMSprop
+from keras import backend as K
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D, Conv2D
+
+batch_size = 128
+num_classes = 10
+epochs = 20
+
+
+# reference : http://enakai00.hatenablog.com/entry/2016/08/02/102917
+class NotMNIST:
+    def __init__(self):
+        images, labels = [], []
+
+        for i, letter in enumerate(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']):
+
+            directory = 'C://Users/Choi Seung Yeon/Desktop/sc/notMNIST_small/%s/' % letter
+
+            files = os.listdir(directory)
+            label = np.array([0] * 10)
+            label[i] = 1
+            for file in files:
+                try:
+                    im = Image.open(directory + file)
+                except:
+                    print
+                    "Skip a corrupted file: " + file
+                    continue
+                pixels = np.array(im.convert('L').getdata())
+                images.append(pixels / 255.0)
+                labels.append(label)
+
+        train_images, test_images, train_labels, test_labels = \
+            train_test_split(images, labels, test_size=0.2, random_state=0)
+
+        class train:
+            def __init__(self):
+                self.images = []
+                self.labels = []
+                self.batch_counter = 0
+
+            def next_batch(self, num):
+                if self.batch_counter + num >= len(self.labels):
+                    batch_images = self.images[self.batch_counter:]
+                    batch_labels = self.labels[self.batch_counter:]
+                    left = num - len(batch_labels)
+                    batch_images.extend(self.images[:left])
+                    batch_labels.extend(self.labels[:left])
+                    self.batch_counter = left
+                else:
+                    batch_images = self.images[self.batch_counter:self.batch_counter + num]
+                    batch_labels = self.labels[self.batch_counter:self.batch_counter + num]
+                    self.batch_counter += num
+
+                return (batch_images, batch_labels)
+
+        class test:
+            def __init__(self):
+                self.images = []
+                self.labels = []
+
+        self.train = train()
+        self.test = test()
+
+        self.train.images = train_images
+        self.train.labels = train_labels
+        self.test.images = test_images
+        self.test.labels = test_labels
+
+
+mnist = NotMNIST()
+
+img_rows, img_cols = 28, 28
+x_train = np.array(mnist.train.images)
+y_train = np.array(mnist.train.labels)
+x_test = np.array(mnist.test.images)
+y_test = np.array(mnist.test.labels)
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+model = Sequential()
+
+model.add(Conv2D(96, kernel_size=(11, 11),
+                 activation='tanh',
+                 input_shape=input_shape))
+
+model.add(Conv2D(256, (5, 5), activation='tanh'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(512, (3, 3), activation='tanh'))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(1024, (3, 3), activation='tanh'))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Conv2D(1024, (3, 3), activation='tanh'))
+
+model.add(Flatten())
+model.add(Dense(3072, activation='tanh'))
+model.add(Dropout(0.5))
+
+model.add(Dense(4096, activation='tanh'))
+model.add(Dropout(0.5))
+
+model.add(Dense(num_classes, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
